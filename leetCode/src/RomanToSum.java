cf508a90d432a6128f15b31a47b270ef9cd61929
Chamreun Roern <160383753+Arius-Chamreun@users.noreply.github.com>
2024-03-21 23:00:34 +0700

    reinter the leetcode answer

---
+++ leetCode/src/RomanToSum.java
@@ -0,0 +1,32 @@
+/**
+ * @github <a href="https://github.com/Arius-Chamreun">Github Url</a>
+ */
+
+public class RomanToSum {
+    public int IsRomanToSum(String string){
+        int value = 0;
+        for(int i = 0 ; i < string.length()  ; i++){
+            switch (string.charAt(i)){
+                case 'I' -> value += 1;
+                case 'V' -> value += 5;
+                case 'X' -> value += 10;
+                case 'L' -> value += 50;
+                case 'C' -> value += 100;
+                case 'D' -> value += 500;
+                case 'M' -> value += 1000;
+            }
+    for(int j = i + 1; j <= string.length(); j++){
+    switch (string.substring(i,j)){
+        case "IV" -> value -= 2;
+        case "IX" -> value -= 2;
+        case "XL" -> value -= 20;
+        case "XC" -> value -= 20;
+        case "CD" -> value -= 200;
+        case "CM" -> value -= 200;
+    }
+
+        }
+    }
+        return value;
+}
+}
---
+++ leetCode/src/TwoSum.java
@@ -0,0 +1,20 @@
+/**
+ * @github <a href="https://github.com/Arius-Chamreun">Github Url</a>
+ */
+
+public class TwoSum {
+    public int[] isTwoSum(int[] num, int target) {
+
+        for (int i = 0; i < num.length; i++) {
+
+            for (int j = i + 1; j < num.length; j++) {
+                if (num[i] + num[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        
+        return new int[0];
+
+    }
+}
